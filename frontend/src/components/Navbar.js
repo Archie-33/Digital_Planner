9081348f3dc9fab16471b0112e1eee778f3bdf98
Archie-33 <archiebhartari@gmail.com>
2023-03-27 12:16:22 +0530

    gl...

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -21,7 +21,7 @@ const Navbar = ({ darkTheme, setDarkTheme }) => {
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <a class="navbar-brand mt-2 mt-lg-0" href="#">
-            <img src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp" height="15" alt="MDB Logo" loading="lazy" />
+            <img src="Euphoriaque_logo.png" height="40" width="40" alt="Euphoriaque" loading="lazy" />
           </a>
 
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
